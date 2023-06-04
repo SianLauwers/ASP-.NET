c2f44cf4d91e1cc3b9f38e711ec845b79adcb45a
Sian Lauwers <r0889620@student.thomasmore.be>
2023-06-04 03:19:47 +0200

    Start

---
+++ www/MotoGP/MotoGP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MotoGP.Models;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MotoGP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Menu()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 5); // Generate a random number between 1 and 4 (inclusive)
+
+            ViewData["BannerNr"] = randomNumber;
+
+            return View();
+        }
+    }
+}
---
+++ www/MotoGP/MotoGP/Controllers/InfoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MotoGP.Models;
+
+namespace MotoGP.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListRaces()
+        {
+            ViewData["BannerNr"] = 0;
+
+            return View();
+        }
+
+        public IActionResult BuildMap()
+        {
+            var races = new List<Race>
+            {
+                new Race { RaceID = 1, X = 517, Y = 19, Name = "Assen" },
+                new Race { RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" },
+                new Race { RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" }
+            };
+
+            ViewData["Races"] = races;
+            ViewData["BannerNr"] = 0;
+
+            return View();
+        }
+    }
+}
