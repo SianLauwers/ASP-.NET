fa947bac78071f7bf22dfcfe5c2dd9b557c3a23a
Sian Lauwers <r0889620@student.thomasmore.be>
2023-06-05 03:54:35 +0200

    MvcMovie ch.3 done

---
+++ www/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult List()
+        {
+            var movies = _context.Movies.OrderBy(m => m.Title);
+
+            return View(movies.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var movie = _context.Movies
+                .Include(m => m.Rating)
+                .SingleOrDefault(m => m.MovieID == id);
+
+            return View(movie);
+        }
+
+    }
+}
--- www/MvcMovie/MvcMovie/Models/Movie.cs
+++ www/MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,10 +1,20 @@
-﻿namespace MvcMovie.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
 {
     public class Movie
     {
         public int MovieID { get; set; }
-        public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Film Title")]
+        public string Title { get; set; }
+
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true,
+          DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; } = string.Empty;
         public int? Price { get; set; }
         public int RatingID { get; set; }
